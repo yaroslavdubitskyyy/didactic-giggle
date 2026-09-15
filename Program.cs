1afc6f375859085eddec2ab73ea5f05a5c4b98ab
yaroslavdubitskyyy <yaroslav_dubitsky@sluz.ch>
2026-09-15 20:45:04 +0200

    Mehrere Dokumente hochgeladen
    Die mensa-aufgabe-C# code, eine screenshot von der Grafik und der yEd-Live Grafik selbst.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15._09._2026PraxisfallMensa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String Artikelname;
+
+            Console.WriteLine("Was willst du kaufen?");
+            Artikelname = Console.ReadLine();
+            //Console.WriteLine(Artikelname);
+            Console.WriteLine("Wie viel kostet es?");
+            decimal Preis = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Wie viel willst du kaufen?");
+            int Menge = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Dein " + Menge + " " + Artikelname + " kostet: " + ((Preis)*(Menge)));
+        }
+    }
+}
